baf7662f3ede4922e028e4012bc7e64c20a7dae1
phermans <phermans@splunk.com>
2017-05-13 16:18:30 -0700

    dev build

--- dist/madlib.bundle.js
+++ dist/madlib.bundle.js
@@ -70,7 +70,19 @@
 /* 0 */
 /***/ (function(module, exports) {
 
-console.log('magic mic');
+var context = new window.AudioContext();
+
+var concatBuffs = function(buff1, buff2) {
+    var numChannels = Math.min( buff1.numberOfChannels, buff2.numberOfChannels );
+    var temp = context.createBuffer( numChannels, (buff1.length + buff2.length), buff1.samplerate);
+
+    for (var i=0; i < numChannels; i++) {
+        var channel = temp.getChannelData(i);
+        channel.set( buff1.getChannelData(i), 0);
+        channel.set( buff2.getChannelData(i), buff1.length);
+    }
+    return temp;
+};
 
 
 /***/ })
