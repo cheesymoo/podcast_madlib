e6478082792cf874fadb02043f5a4b7f7cea21cf
gnarvaja <guillermo.narvaja@radiocut.fm>
2017-05-14 17:19:15 -0300

    Fixes

--- backend/csv2madlib.py
+++ backend/csv2madlib.py
@@ -6,8 +6,9 @@ rows = list(csv.reader(open("Questions.csv")))[1:]
 
 for i, row in enumerate(rows):
     text = open("madlibs/madlib.yaml.template").read()
-    if not os.path.exists("media/%s.mp3" % row[0]):
-        print("Error line %d: %s does not exists!" % (i + 1, row[0]))
+    filename = row[0].strip()
+    if not os.path.exists("media/%s.mp3" % filename):
+        print("Error line %d: %s does not exists!" % (i + 1, filename))
         continue
-    new_yaml = text.format(question=row[2], interviewer=row[1], filename=row[0])
+    new_yaml = text.format(question=row[2], interviewer=row[1], filename=filename)
     open("madlibs/q%d.yaml" % i, "wt").write(new_yaml)
